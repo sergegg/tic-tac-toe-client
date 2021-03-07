7ba06461228cdf00343b91496562257512adb0da
sergey honiyan <sergehoniyan@gmail.com>
2021-03-06 22:09:31 -0500

    fixing styling errors

--- assets/scripts/games/events.js
+++ assets/scripts/games/events.js
@@ -8,7 +8,6 @@ const player1 = 'X'
 const player2 = 'O'
 let currentPlayer = player1
 let isOver = false
-// let currentPlayer = ''
 const onGetGames = function (event) {
   event.preventDefault()
   api.index()
@@ -18,39 +17,17 @@ const onGetGames = function (event) {
 
 const onCreateGame = function () {
   event.preventDefault()
-  // the only thing that needs to be set is player X to start again.
-  // afterwards, the new and clean game object is sent in onCreateGameSuccess
   currentPlayer = player1
-  // need to reset the 'X's and O's to be '' again!
   api.createGame()
     .then(ui.onCreateGameSuccess)
     .catch(ui.onError)
 }
 
 const onGameUpdate = function (event) {
-  // prevent refreshing of page
   event.preventDefault()
-  // console.log('what is event?? ', event)
-  // console.log('does store exist? ', store)
-  // find the cell index to the current box which is clicked
   const cellIndex = $(this).data('cell-index')
-  // console.log('What is cell index?? ', cellIndex)
   const currentBox = event.target
-  // find the box which the user clicked using the event which was passed in.
-  // is the box text is matching and the game isn't over, fill in the box
-  // with the current player and start checking if there is a winner..
-  // based on the result of the search, send the information to the api.
-  // store.game.over is checked, if its false (resets after game object is sent)
-  // then continue filling in the boxes as regular.
   if ($(currentBox).html() === '' && store.game.over === false) {
-    // i need to check if the board is full.. to tell user the game is over, start new
-    // if (store.game.over === false && (store.game.cells[cellIndex] === player1 || player2)) {
-    //   $('#message2').text('box is taken or game is over, start a new game if over!')
-    // }
-    // if ($(currentBox).html() !== '' && store.game.over !== true) {
-    //   $('#message2').text('The game is a tie! No winner')
-    // }
-    // need to create a logic where the game lets you know if there is a tie..
     $(currentBox).html(currentPlayer)
     store.game.cells[cellIndex] = currentPlayer
     if ((store.game.cells[0] === currentPlayer) &&
@@ -102,16 +79,11 @@ const onGameUpdate = function (event) {
       $('#message2').text(`Congragulations ${currentPlayer}, you win!`)
     } else {
       isOver = false
-      // update the player to be the next one to move if game isn't over
       $('#message2').text('No winner yet, next player.. ')
     }
-    // console.log('what is store.game.celsl ', store.game.cells)
-    // console.log('whos the current player ', currentPlayer)
     api.gameUpdate(cellIndex, currentPlayer, isOver)
       .then(ui.onGameUpdateSuccess)
-      // .then(ui.onGameOver)
       .catch(ui.onError)
-      // if ? if true : if false
     currentPlayer = currentPlayer === player1 ? player2 : player1
   } else if (store.game.cells[cellIndex] !== '') {
     $('#message2').text('No can do, you are trying to put your mark on a box which is already selected.. Please try again :) ')
--- assets/scripts/games/ui.js
+++ assets/scripts/games/ui.js
@@ -1,8 +1,6 @@
 'use strict'
 
 const store = require('./../store')
-// require the logic for the gameboard
-// const logic = require('./logic')
 
 const onError = function (error) {
   $('#message2').text('Failed to execute, please try again or fix the code. ', error)
@@ -10,31 +8,16 @@ const onError = function (error) {
 const onCreateGameSuccess = function (event) {
   $('#message2').text('New game Created. Player "X" goes first. Have fun & dont cheat 8)!')
   $('#game-table').show()
-  // event object is stored inside the API
-  // console.log('event.target is --- ', event.target) // undefined
-  // console.log('event is IN UI ', event)
-  // // console.log('event.html?? ', event.game.html) // undefined
-  // console.log('event.game is IN UI ', event.game) // game object is event.game
-  // console.log('what is store -- IN UI --', store)
-  // event.over = false
   $('.col-4').html('')
   store.game = event.game
-  // console.log('current is', event)
 }
 
 const onGameUpdateSuccess = function (event) {
-  // $('#message2').show()
-  // simply store the current position of players into the data.
   store.game = event.game
-  // console.log('hello, are you here event? ', event) // the game object
-  // console.log('store.game huh? ', store.game) // game object
 }
 
 const onGetGamesSuccess = function (event) {
-  // set games to the current total length of games created
   const games = event.games.length
-  // console.log('what is gameS?? ', games)
-  // display the total amount of games played.
   $('#view-games').html(`The total games you've played and created is: ${games}`)
 }
 
